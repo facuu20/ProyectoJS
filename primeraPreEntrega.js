99a5379e6ce01f5688f06f93c9b42c20b0e0833f
facuu20 <139825792+facuu20@users.noreply.github.com>
2023-10-31 22:57:24 -0300

    bucle for y funciones

--- primeraPreEntrega.js
+++ primeraPreEntrega.js
@@ -1,14 +1,34 @@
-let numeroRandom = Math.floor(Math.random() * 100) + 1;
-console.log(numeroRandom)
-let numero = parseInt(prompt('Ingrese su numero'));
-while (numeroRandom != numero) {
 
-    if (numeroRandom > numero) {
-        console.log('El numero elegido es menor');
-
+function juego() {
+    
+    let numeroRandom = Math.floor(Math.random() * 100) + 1;
+    let numero = null
+    console.log(numeroRandom)
+    if (numero == null) {
+    numero = parseInt(prompt('Ingrese su numero'))
+}
+    
+    while (numeroRandom != numero) {
+        
+        if (numeroRandom < numero) {
+            numero = parseInt(prompt("es menor"));
+        }
+        else ( 
+            
+            numero= parseInt(prompt("es mayor")))
+            
     }
-    else {
-        console.log('El numero es mayor');
+    alert('Le pegaste rey')
+}
+
+function bucleFor() {
+    
+    alert('Abri la consola')
+    
+    let numeros= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12 ]
+    for (let index = 0; index < 10; index++) {
+        const element = numeros[index];
+        console.log('Este numero no es mayor a 10 mira: ' + index)
     }
 }
-console.log('Le pegaste rey')
+    
